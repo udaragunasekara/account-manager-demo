7f9415bb2260205959a29b23a8491e3a9c11aa54
Udara Gunasekara <udara.gunasekara@acularo.com>
2019-09-27 12:53:42 +1000

    #1 feat: adds spring starter generated files

---
+++ src/main/java/com/ug/accountmanagerdemo/AccountManagerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ug.accountmanagerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagerDemoApplication.class, args);
+	}
+
+}
